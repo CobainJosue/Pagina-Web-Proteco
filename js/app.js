2827a8dc8f9c8462df61d43222e476774b941de8
Josue <chivasagrada@live.com.mx>
2017-05-01 12:29:20 -0500

    Input para buscar curso (incompleto)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+(function(){
+	'use strict';
+	angular
+	.module("myApp",[])		//El segundo parametro son las dependencias a usar (firebase, para api's, etc)
+	.controller("cursosCtrl",cursosCtrl);
+	function cursosCtrl(){
+		var cursos = this;
+
+		cursos.id = [{'id':'C Basico':}
+						{'id': 'C Intermedio'}
+										]
+	}
+})();
+
 $(document).ready(function(){
 	$(".dropdown-button").dropdown();
 	$('.button-collapse').sideNav();
