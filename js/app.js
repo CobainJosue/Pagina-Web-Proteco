c9d93e505f29e09acbd515627388500313424ac2
Josue <chivasagrada@live.com.mx>
2017-05-01 23:02:25 -0500

    Busqueda de cursos funcionando

--- js/app.js
+++ js/app.js
@@ -6,9 +6,29 @@
 	function cursosCtrl(){
 		var cursos = this;
 
-		cursos.id = [{'id':'C Basico':}
-						{'id': 'C Intermedio'}
-										]
+		cursos.info = [
+						{ 'id':'C Basico', 'imagen':'img/c.png' },
+						{'id':'C Intermedio','imagen':'img/c.png'},
+						{'id':'C Avanzado','imagen':'img/c.png'},
+						{'id':'Java Basico','imagen':'img/java.png'},
+						{'id':'Java Intermedio','imagen':'img/java.png'},
+						{'id':'Java Avanzado','imagen':'img/java.png'},
+						{'id':'C ++ Basico','imagen':'img/cmasmas.png'},
+						{'id':'C ++ Intermedio', 'imagen':'img/cmasmas.png'},
+						{'id':'C ++ Avanzado', 'imagen':'img/cmasmas.png'},
+						{'id':'Matlab Basico','imagen':'img/matlab.png'},
+						{'id':'Matlab Intermedio','imagen':'img/matlab.png'},
+						{'id':'Matlab Avanzado','imagen':'img/matlab.png'},
+						{'id':'Python Basico','imagen':'img/python.png'},
+						{'id':'Python Intermedio','imagen':'img/python.png'},
+						{'id':'Python Avanzado','imagen':'img/python.png'},
+						{'id':'Inteligencia Artificial Basico','imagen':'img/inteligencia.png'},
+						{'id':'Inteligencia Artificial Intermedio','imagen':'img/inteligencia.png'},
+						{'id':'Inteligencia Artificial Avanzado','imagen':'img/inteligencia.png'},
+						{'id':'Raspberry Pi','imagen':'img/raspberry.png'},
+						{'id':'Raspberry Pi','imagen':'img/raspberry.png'},
+						{'id':'Raspberry Pi','imagen':'img/raspberry.png'},
+					];
 	}
 })();
 
